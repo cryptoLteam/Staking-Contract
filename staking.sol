3ce46004a9021b585c8635096eb165f92f58cf65
Blockchain Expert <iamgirldev@gmail.com>
2021-11-11 20:58:46 +0900

    Version1.0

---
+++ staking.sol
@@ -0,0 +1,8 @@
+staking contract address
+https://testnet.bscscan.com/address/0xab6ef2576afcD7598E4DCfCA01215fEeA2a07C91#writeContract
+
+airdrop contract address
+https://testnet.bscscan.com/address/0x71e8ec88826a47f24631dc2f01085e98b76685da#writeContract
+
+Reward token contract address
+https://testnet.bscscan.com/address/0x30ba2db859e2f998a82cb9485b0b288aea48651a#writeContract
